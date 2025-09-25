7799dbaf7e23e7fb5cfa51f936358fc0bbb3b09d
Charana Sukrot <charana.sukrot@agoda.com>
2025-09-25 11:55:05 +0700

    refactor: icon and gap

--- app/record/page.tsx
+++ app/record/page.tsx
@@ -772,10 +772,12 @@ export default function RecordPage() {
                 checked={cameraDisabled}
                 onChange={(e) => setCameraDisabled(e.target.checked)}
               />
-              Disable camera (testing)
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera-video-off" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M10.961 12.365a2 2 0 0 0 .522-1.103l3.11 1.382A1 1 0 0 0 16 11.731V4.269a1 1 0 0 0-1.406-.913l-3.111 1.382A2 2 0 0 0 9.5 3H4.272l.714 1H9.5a1 1 0 0 1 1 1v6a1 1 0 0 1-.144.518zM1.428 4.18A1 1 0 0 0 1 5v6a1 1 0 0 0 1 1h5.014l.714 1H2a2 2 0 0 1-2-2V5c0-.675.334-1.272.847-1.634zM15 11.73l-3.5-1.555v-4.35L15 4.269zm-4.407 3.56-10-14 .814-.58 10 14z"/>
+              </svg>
             </label>
           </div>
-          <div className="aspect-video w-full bg-black/80 rounded overflow-hidden flex items-center justify-center">
+          <div className="aspect-video w-full bg-black/80 rounded overflow-hidden flex items-center justify-center mt-4">
             <video
               ref={videoRef}
               id="record-video"
